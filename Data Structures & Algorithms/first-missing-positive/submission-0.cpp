d9d5eecd50149bacca4af1566a1095db7b60a997
Aryan Kaundilya <aryankaundilya.jas@gmail.com>
2026-08-30 14:07:12 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int missing =1;
+        while(true){
+            bool flag= true;
+            for(int& num : nums){
+                if(missing==num){
+                    flag= false;
+                    break;
+                }
+            }
+            if(flag) return missing;
+            missing++;
+        }
+        
+        
+    }
+};
