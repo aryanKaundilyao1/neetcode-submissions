33e0e13d90574d27f9850deeaad46104f89cb3d3
Aryan Kaundilya <aryankaundilya.jas@gmail.com>
2026-08-30 13:43:57 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+        int count=0;
+        for(int num : nums){
+            int count=0;
+            for(int i : nums){
+                if(i==num){
+                    count++;
+                }
+            }
+            if(count> n/2){
+                return num;
+            }
+        }
+        return -1;
+    }
+};
